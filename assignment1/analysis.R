d7cb9cb41d50dd99e6e5caa93809acca05aba08c
Michael Bessey <besseym@gmail.com>
2015-08-01 21:33:19 -0400

    complete analysis and finished charts

--- assignment1/analysis.R
+++ assignment1/analysis.R
@@ -6,6 +6,7 @@ setwd("/Users/besseym/Personal/Education/Coursera/DataMining/DataVisualization/p
 library(reshape)
 library(ggplot2)
 library(plyr)
+library(scales)
 
 df <- read.table("data/ExcelFormattedGISTEMPDataTXT.txt", header = TRUE, sep = "\t", strip.white=TRUE, na.strings=c("***", "****", "", "NA"))
 head(df)
@@ -15,72 +16,54 @@ summary(df)
 
 v_names <- names(df)
 
-?apply
-l_has_missing <- apply(df, 1, function(x){sum(is.na(x)) <= 0})
-
-df <- df[l_has_missing,]
-
-
 df_monthly <- df[,c("Year", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")]
 head(df_monthly)
-df_monthly_m <- melt(df_monthly, id=c("Year"), variable_name="Month")
-head(df_monthly_m)
 
-write.csv(df_monthly_m, file = "monthly.csv")
+l_has_missing <- apply(df_monthly, 1, function(x){sum(is.na(x)) > 0})
 
-l_decade <- df_monthly_m$Year > 1900 & (df_monthly_m$Year %% 10) == 0
-df_monthly_decade <- df_monthly_m[l_decade,]
+df_mc <- df_monthly[!l_has_missing,]
+head(df_mc)
 
-df_monthly_sum_by_year <- ddply(df_monthly_m,.(Year),summarize,value=sum(value))
-df_monthly_sum_by_year
-ddply(df_monthly_m,.(Month),summarize,value=sum(value))
-ddply(df_monthly_m,.(Month),summarize,value=mean(value))
+df_mc_m <- melt(df_mc, id=c("Year"), variable_name="Month")
+head(df_mc_m)
+names(df_mc_m) <- c('Year', 'Month', 'Temp')
 
-#Decade by Month
-v_year <- unique(df_monthly_decade$Year)
-v_year[order(v_year, decreasing = TRUE)]
-f_year <- factor(df_monthly_decade$Year, levels = v_year[order(v_year, decreasing = TRUE)])
-p <- ggplot(df_monthly_decade, aes(x=Month, y=value, group=Year))
+write.csv(df_mc_m, file = "monthly.csv")
+
+p <- ggplot(df_mc_m, aes(x=Year, y=Temp))
+p <- p + geom_line(size = 1, aes(colour = Temp))
+#p <- p + scale_color_gradient2(low = muted("blue"), mid = "grey", high = muted("red"), midpoint = 0, space = "rgb", guide = "colourbar")
+#p <- p + scale_colour_brewer("RdBu")
+p <- p + scale_color_gradient2(name = "Temp. in °C", low = "dodgerblue3", mid = "lightyellow3", high = "firebrick3", midpoint = 0, space = "rgb", guide = "colourbar")
+p <- p + facet_grid(Month ~ .) +  facet_wrap( ~ Month, ncol=2) + theme_bw()
+p <- p + labs(x = "Year", y = "Temperature in °C") 
+p
+
+saveChart(p, "temp_by_month", w = 12, h = 9)
+
+
+l_decade <- df_mc_m$Year > 1900 & (df_mc_m$Year %% 10) == 0
+df_decade <- df_mc_m[l_decade,]
+head(df_decade)
+
+v_year <- unique(df_decade$Year)
+f_year <- factor(df_decade$Year, levels = v_year[order(v_year, decreasing = TRUE)])
+
+p <- ggplot(df_decade, aes(x=Month, y=Temp, group=f_year))
 p <- p + geom_line(size = 1.5, aes(colour = f_year))
 p <- p + geom_point(size = 2.5, aes(colour = f_year))
-p <- p + scale_colour_brewer(palette="Paired")
-#p <- p + geom_text(aes(label = value), vjust=-0.8)
-p <- p + labs(title = "Total Average Temperature Across All Zones per Decade", x = "Month", y = "Temperature") 
-p <- p + theme_grey()
+p <- p + labs(x = "Month", y = "Temp. in °C") 
+#p <- p + theme_bw()
+#p <- p + scale_colour_brewer(palette="Paired")
+p <- p + theme_grey() + guides(colour=guide_legend(title="Year"))
 p
+
+?legend
 
 saveChart(p, "decade_by_month")
 
-#ggplot(df_monthly_m, aes(x=Month, y=value, group=Month)) + geom_line(aes(colour = Year))
 
-# distribution of zones
-ggplot(df_monthly_m, aes(Month, value)) + geom_boxplot() + coord_flip()
-ggplot(df_monthly_m, aes(x=value)) + geom_histogram() + facet_grid(. ~ Month)
-
-
-df_zone <- df[,c("Year", "J.D", "D.N", "DJF", "MAM", "JJA", "SON")]
-head(df_zone)
-
-df_zone_m <- melt(df_zone, id=c("Year"), variable_name="Zone")
-head(df_zone_m)
-
-write.csv(df_zone_m, file = "zone.csv")
-
-ggplot(df_zone_m, aes(x=Year, y=value)) + geom_line(aes(colour = Zone))
-ggplot(df_zone_m, aes(x=Year, y=value, group=Zone)) + geom_point(aes(colour = Zone)) + annotate("text", label = "plot mpg vs. wt", x = 2000, y = 50, size = 8, colour = "red")
-ggplot(df_zone_m, aes(x=Year, y=value, group=Zone)) + geom_line() + facet_grid(. ~ Zone)
-ggplot(df_zone_m, aes(x=Year, y=value, group=Zone)) + geom_line(aes(colour = Zone))
-
-ggplot(df_zone_m, aes(x=value)) + geom_histogram() + facet_grid(. ~ Zone)
-
-unique(df_zone_m$Zone)
-
-# distribution of zones
-ggplot(df_zone_m, aes(Zone, value)) + geom_boxplot()
-
-df_zone_sum_by_year <- ddply(df_monthly_m,.(Year),summarize,value=sum(value))
-
-sum(df_zone_sum_by_year$value != df_monthly_sum_by_year$value)
+ggplot(df_decade, aes(x=Temp)) + geom_histogram() + facet_grid(. ~ Month)
 
 
 saveChart <- function(p, name, w = 16.18, h = 10){
--- assignment1/analysis1.R
+++ assignment1/analysis1.R
@@ -20,8 +20,32 @@ df_hem <- df[,c("Year", "Glob", "NHem", "SHem")]
 head(df_hem)
 df_hem_m <- melt(df_hem, id=c("Year"), variable_name="Hem")
 head(df_hem_m)
+unique(df_hem_m$Hem)
 
-ggplot(df_hem_m, aes(x=Year, y=value)) + geom_line(aes(colour = Hem))
+df_glob <- df_hem_m[df_hem_m$Hem == 'Glob',]
+df_nhem <- df_hem_m[df_hem_m$Hem == 'NHem',]
+df_shem <- df_hem_m[df_hem_m$Hem == 'SHem',]
+
+l_filter <- df_hem_m$Hem == 'Glob' & (df_hem_m$Year %% 10) == 0
+df_glob_decade_agg <- ddply(df_hem_m[l_filter, ], .(Year), summarize, Mean_Temp = mean(value))
+head(df_glob_decade_agg)
+
+c(253,192,134) / 255
+
+c_alpha <- 0.3
+
+p <- ggplot()
+p <- p + geom_line(data = df_hem_m, size = 1.5, aes(x=Year, y=value, group=Hem, color=Hem))
+p <- p + scale_colour_manual(name = "Location", values = c("Glob" = rgb(0.4980392, 0.7882353, 0.4980392,1.0), "NHem" = rgb(0.9921569, 0.7529412, 0.5254902, c_alpha), "SHem" = rgb(0.7450980, 0.6823529, 0.8313725, c_alpha)), labels = c("Glob" = "Global", "NHem" = "N. Hemisphere", "SHem" = "S. Hemisphere"))
+p <- p + geom_point(data = df_glob_decade_agg, size = 5, aes(x=Year, y=Mean_Temp)) 
+p <- p + geom_text(data = df_glob_decade_agg, aes(x = Year, y = Mean_Temp, family="Arial", fontface="italic", label = paste(Mean_Temp, "°C in ", Year, sep = ""), hjust=0.3, vjust=-1.0))
+p <- p + labs(x = "Year", y = "Temperature in °C")
+p <- p + theme_bw() # + guides(colour=guide_legend(title="Avg. Temp in given Year"))
+p <- p + theme(legend.key = element_rect(size = 0), panel.border = element_rect(size = 0))
+#p <- p + annotate("text", label = "Points indicate average temperature in specified year.", x = median(df_hem_m$Year), y = 75, size = 5)
+p
+
+saveChart(p, "temp_total")
 
 
 df_loc <- df[,c("Year", "X24N.90N", "X24S.24N", "X90S.24S", "X64N.90N", "X44N.64N", "X24N.44N", "EQU.24N", "X24S.EQU", "X44S.24S", "X64S.44S", "X90S.64S")]
@@ -64,14 +88,19 @@ df_temp_range <- df_loc_m[df_loc_m$Year %% 100 == 0, ]
 
 df_loc_m$Year > 1949
 
-df_temp_range <- df_loc_m[df_loc_m$Year > 1949 & df_loc_m$Year %% 10 == 0 & df_loc_m$Loc %in% c('X64N.90N', 'X44N.64N', 'X24N.44N', 'EQU.24N', 'X24S.EQU', 'X44S.24S', 'X64S.44S', 'X90S.64S'), ]
-df_temp_range
+l_lat_cols <- df_loc_m$Loc %in% c('X64N.90N', 'X44N.64N', 'X24N.44N', 'EQU.24N', 'X24S.EQU', 'X44S.24S', 'X64S.44S', 'X90S.64S')
+l_restict_year <- df_loc_m$Year > 1900
+l_mod_year <- df_loc_m$Year %% 10 == 0
+sum(l_restict_year  & l_mod_year)
+
+df_temp_range <- df_loc_m[l_lat_cols & l_mod_year, ]
+length(unique(df_temp_range$Year))
 
 
 ggplot(df_temp_range, aes(x = factor(1), y = lat2, fill = value)) + geom_bar(stat = "identity")
 ggplot(df_temp_range, aes(x= factor(1), y= lat2, fill=value)) + geom_area(stat="identity")
 
-ggplot(df_temp_range) + geom_ribbon(stat="bin", aes(x = 1, ymin = lat1, ymax = lat2, group = Loc, fill=value), alpha= 0.5) + scale_fill_gradient2(low = muted("blue"), mid = "white", high = muted("red"), midpoint = 0, space = "rgb", guide = "colourbar") + facet_grid(. ~ Year)
+ggplot(df_temp_range) + geom_ribbon(stat="bin", aes(x = 1, ymin = lat1, ymax = lat2, group = Loc, fill=value), alpha= 0.5) + scale_fill_gradient2(low = muted("blue"), mid = "white", high = muted("red"), midpoint = 0, space = "rgb", guide = "colourbar") + facet_grid(. ~ Year) + facet_wrap( ~ Year, ncol=7)
 
 ?aes
 ?geom_ribbon
@@ -81,16 +110,17 @@ library(maps)
 library(ggplot2)
 map.data <- map_data("world")
 ggplot() + geom_polygon(aes(long,lat, group=group), fill="grey65", data=map.data) + theme_bw()
-ggplot() + geom_polygon(aes(long,lat, group=group), fill="grey65", data=map.data) + theme_bw() + geom_ribbon(data = df_temp_range, stat="bin", aes(x = 1, ymin = lat1, ymax = lat2, group = Loc, fill=value), alpha= 0.5) + scale_fill_gradient2(low = muted("blue"), mid = "white", high = muted("red"), midpoint = 0, space = "rgb", guide = "colourbar") + facet_grid(. ~ Year)
-?geom_ribbon
 
+p <- ggplot()
+p <- p + geom_polygon(aes(long,lat, group=group), fill="grey65", data=map.data)
+p <- p + theme_bw()
+p <- p + geom_ribbon(data = df_temp_range, stat="bin", aes(x = 1, ymin = lat1, ymax = lat2, group = Loc, fill=value), alpha= 0.5) 
+p <- p + scale_fill_gradient2(name = "Temp. in °C", low = "dodgerblue3", mid = "lightyellow3", high = "firebrick3", midpoint = 0, space = "rgb", guide = "colourbar") 
+p <- p + facet_grid(. ~ Year) + facet_wrap( ~ Year, ncol=5)
+p <- p + labs(y = "Latitude") 
+p <- p + theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.title.x=element_blank())
+p
 
-
-
-huron <- data.frame(year = 1875:1972, level = as.vector(LakeHuron))
-huron$decade <- round_any(huron$year, 10, floor)
-head(huron)
-
-ggplot(huron, aes(x=year)) + geom_ribbon(aes(ymin=0, ymax=level))
+saveChart(p, "temp_by_latitude")
 
 
